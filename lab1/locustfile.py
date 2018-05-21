5c4151626318b2a9fb45cb6ebd14372af62c2449
Daniel Ives <dives@qa.com>
2018-05-21 13:44:36 +0100

    added some randomness to locustfile

--- lab1/locustfile.py
+++ lab1/locustfile.py
@@ -1,18 +1,46 @@
 from locust import HttpLocust, TaskSet
+import random
+
+PATHS = [
+    "Atlanta_GA",
+    "Boston_MA",
+    "Charlotte_NC",
+    "Chicago_IL",
+    "Dallas_TX",
+    "Denver_CO",
+    "Detroit_MI",
+    "Fort_Lauderdale_FL",
+    "Houston_TX",
+    "Las_Vegas_NV",
+    "Los_Angeles_CA",
+    "Miami_FL",
+    "Minneapolis_MN",
+    "New_York_NY",
+    "Orlando_FL",
+    "Philadelphia_PA",
+    "Phoenix_AZ",
+    "Portland_OR",
+    "Salt_Lake_City_UT",
+    "San_Diego_CA",
+    "San_Francisco_CA",
+    "Seattle_WA",
+    "Tampa_FL",
+    "Washington_D.C.",
+]
 
 def index(l):
     l.client.get("/")
 
-def query(l, path):
-    l.client.get("/" + path)
+def query(l):
+    l.client.get("/" + random.choice(PATHS))
 
 class UserBehavior(TaskSet):
-    tasks = {index: 1, query: 1}
+    tasks = {index: 1, query: 4}
 
     def on_start(self):
         pass
 
 class WebsiteUser(HttpLocust):
     task_set = UserBehavior
-    min_wait = 5000
+    min_wait = 2000
     max_wait = 9000
